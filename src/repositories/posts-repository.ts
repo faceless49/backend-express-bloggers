9a05380964ab058c0ef533532bca70690bfc196a
faceless49 <digital.zeus@mail.ru>
2022-04-19 10:04:55 +0300

    fixed(posts-repo): Change condition

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -1,5 +1,5 @@
-import {BloggerType, PostType} from '../types';
-import {bloggersRepository} from './bloggers-repository';
+import { BloggerType, PostType } from '../types';
+import { bloggersRepository } from './bloggers-repository';
 
 const posts: PostType[] = [
   {
@@ -20,14 +20,18 @@ const posts: PostType[] = [
   },
 ];
 
-
 export const postsRepository = {
   async findPosts(): Promise<PostType[]> {
-    return posts
+    return posts;
   },
-  async createPost(title: string, shortDescription: string, content: string, bloggerId: number): Promise<PostType | undefined> {
+  async createPost(
+    title: string,
+    shortDescription: string,
+    content: string,
+    bloggerId: number,
+  ): Promise<PostType | undefined> {
     const bloggers = await bloggersRepository.getAllBloggers();
-    const blogger = bloggers.find(({id}) => id === bloggerId)
+    const blogger = bloggers.find(({ id }) => id === bloggerId);
     if (blogger) {
       const newPost: PostType = {
         id: +new Date(),
@@ -38,38 +42,42 @@ export const postsRepository = {
         bloggerName: blogger.name,
       };
       posts.push(newPost);
-      return newPost
+      return newPost;
     }
   },
 
   async findPostById(postId: number): Promise<PostType | undefined> {
-    return posts.find((p) => p.id === postId)
+    return posts.find(p => p.id === postId);
   },
 
-  async updatePostById(id: number, title: string, shortDescription: string, content: string, bloggerId: number): Promise<number> {
-    const post = posts.find((p) => p.id === id);
-    const blogger: BloggerType | undefined = (await bloggersRepository.getAllBloggers()).find(({id}) => post?.bloggerId === id)
-    if(!post) {
-      return 400
-    }
-    if (!blogger) {
-      return 400
+  async updatePostById(
+    id: number,
+    title: string,
+    shortDescription: string,
+    content: string,
+    bloggerId: number,
+  ): Promise<number> {
+    const post = posts.find(p => p.id === id);
+    const blogger: BloggerType | undefined = (
+      await bloggersRepository.getAllBloggers()
+    ).find(({ id }) => post?.bloggerId === id);
+    if (!post || !blogger) {
+      return 400;
     } else {
       post.title = title;
       post.shortDescription = shortDescription;
       post.content = content;
-      post.bloggerId = bloggerId
-      return 204
+      post.bloggerId = bloggerId;
+      return 204;
     }
   },
   async deletePostById(id: number): Promise<boolean> {
     for (let i = 0; i < posts.length; i++) {
       if (posts[i].id === id) {
-        posts.splice(i, 1)
-        return true
+        posts.splice(i, 1);
+        return true;
       }
     }
-    return false
-  }
-
-}
+    return false;
+  },
+};
